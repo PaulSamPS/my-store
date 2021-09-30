f6ff8883902ec8c5ef536138745df4b17e6379b0
PaulSamPS <psamoylenko2@gmail.com>
2021-09-30 03:16:14 +0300

    контекст

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,31 +3,32 @@ import { Header } from "./Header/Header"
 import { Sidebar } from "./Sidebar/Sidebar"
 import { Footer } from "./Footer/Footer"
 import styles from './Layout.module.scss'
+import { FunctionComponent } from "react"
+import { AppContextProvider, IAppContext } from "../context/app.context"
 import cn from 'classnames'
-import {FunctionComponent} from "react";
 
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-       <>
-           <Header />
-           <div>
-               <Sidebar />
-               <div>
-                   { children }
-               </div>
+       <div className={styles.wrapper}>
+           <Header className={styles.header}/>
+           <Sidebar className={styles.sidebar}/>
+           <div className={styles.body}>
+               { children }
            </div>
-           <Footer />
-       </>
+           <Footer className={styles.footer}/>
+       </div>
     )
 }
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
         return (
-            <Layout>
-                <Component { ...props }/>
-            </Layout>
+            <AppContextProvider menu={ props.menu } firstCategory={ props.firstCategory }>
+                <Layout>
+                    <Component { ...props }/>
+                </Layout>
+            </AppContextProvider>
         )
     }
 }
